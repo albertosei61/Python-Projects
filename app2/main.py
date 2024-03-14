c89c5b8504da9244b56caa2d6af51c6a08ba4697
Albert Osei <albertosei61@gmail.com>
2024-03-13 22:47:55 -0400

    Page setup(add images and description)

---
+++ app2/main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo1.png")
+
+with col2:
+    st.title("Albert Osei")
+    content = """I am a Python programmer"""
+    st.info(content)
+body = st.text("Below you can find some of the apps I have built in Python. Feel free to contact me!")
