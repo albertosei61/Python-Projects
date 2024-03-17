c37c7d7083e9def63a3b19f49f7abd2d6596d2c7
Albert Osei <albertosei61@gmail.com>
2024-03-16 23:51:19 -0400

    Created topic section & Encrypted password for email function for contact form

--- app2/pages/Contact_Us.py
+++ app2/pages/Contact_Us.py
@@ -1,17 +1,24 @@
 import streamlit as st
 from send_email import email_conn
+import pandas as pd
 
 
+df = pd.read_csv("topics.csv")
 st.header("Contact Me")
 
 with st.form(key="email_forms"):
     user_email = st.text_input("Your email address")
+
+    for row in df:
+        topic_choice = st.selectbox("What topic do you want to discuss?", df["topic"] )
+
     raw_message = st.text_area("Your message")
     message = f"""\
 Subject: New email from {user_email}
-
+{topic_choice}
 From: {user_email}
-{raw_message}    
+{raw_message}
+  
 """
     button = st.form_submit_button("Submit")
     if button:
--- app2/send_email.py
+++ app2/send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 def email_conn(message):
     host = "smtp.gmail.com"
     port = 465
 
     username = "atwenefour21@gmail.com"
-    password = "wvdr godl rhot pmkg"
+    password = os.getenv("PASSWORD_PORTFOLIO_APP")
 
     receiver = "atwenefour21@gmail.com"
     context = ssl.create_default_context()
