9fa31f4d02ce42c3b822f11f61a24c1f4c890da4
Albert Osei <albertosei61@gmail.com>
2024-03-16 22:52:52 -0400

    Email contact form function completed

--- app2/send_email.py
+++ app2/send_email.py
@@ -1,18 +1,15 @@
 import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
+def email_conn(message):
+    host = "smtp.gmail.com"
+    port = 465
 
-username = "atwenefour21@gmail.com"
-password = "wvdr godl rhot pmkg"
+    username = "atwenefour21@gmail.com"
+    password = "wvdr godl rhot pmkg"
 
-receiver = "atwenefour21@gmail.com"
-context = ssl.create_default_context()
+    receiver = "atwenefour21@gmail.com"
+    context = ssl.create_default_context()
 
-message = """ Hi, How are you?
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
-
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
