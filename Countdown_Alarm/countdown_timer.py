237aacd3c8138be12b4c6ce0109843cd5445da88
Albert Osei <albertosei61@gmail.com>
2024-05-31 21:23:40 -0400

    Added UI/UX features - Timer descriptions

--- Countdown_Alarm/countdown_timer.py
+++ Countdown_Alarm/countdown_timer.py
@@ -1,5 +1,6 @@
 import time
 import winsound
+import tqdm
 
 # Function to convert time format to seconds
 def get_seconds(time_str):
@@ -7,11 +8,15 @@ def get_seconds(time_str):
     return h * 3600 + m * 60 + s
 
 # Countdown function
-def countdown(t_second, message):
+def countdown(t_second, message, period_type):
     while t_second:
         mins, secs = divmod(t_second, 60) 
         hours, mins = divmod(mins, 60)
-        timeformat = '{:02d}:{:02d}:{:02d}'.format(hours, mins, secs) 
+        if period_type == 'work':
+            timeformat = 'Work time remaining: {:02d}:{:02d}:{:02d}'.format(hours, mins, secs) 
+        else:
+            timeformat = 'Rest time remaining: {:02d}:{:02d}:{:02d}'.format(hours, mins, secs) 
+
         print(timeformat, end='\r')
         time.sleep(1)   
         t_second -= 1
@@ -27,11 +32,11 @@ def stop_alarm():
 def cycle(work_seconds, rest_seconds, cycles):
     for _ in range(cycles):
         # Running the work timer
-        countdown(work_seconds, "Work Session Over. Time to rest!")
+        countdown(work_seconds, "Work Session Over. Time to rest!", 'work')
         stop_alarm()
 
         # Running the rest timer
-        countdown(rest_seconds, "Rest Session Over. Time to get back to work!")
+        countdown(rest_seconds, "Rest Session Over. Time to get back to work!", 'rest')
         stop_alarm()
 
 
@@ -41,10 +46,13 @@ rest_timer = input("Set Rest Timer: Enter a time to rest in the format HH:MM:SS\
 
 #Asking for number of cycle
 cycle_input = int(input("Enter number of cycle: "))
-
+  
 # Converting time format to seconds
 work_seconds = get_seconds(wk_timer)
 rest_seconds = get_seconds(rest_timer)
 
 # Starting the Pomodoro cycles
 cycle(work_seconds, rest_seconds, cycle_input)
+
+#New Feature- Create an option to pause work and rest timer.#
+#New Feature - Create an option to stop pomodoro in the middle of timer
